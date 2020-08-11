0e3cdec6a8edef6e251a35ed3e3efdb5382d871c
Mayank Raj <AgrMayank@outlook.com>
2020-08-12 00:34:21 +0530

    Add Rotate Scripts; Fix Lights

---
+++ Assets/Scripts/SwipeRotate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+ 
+public class SwipeRotate : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 touchPosition;
+    private Quaternion rotY;
+    public float rotateSpeedModifier = 0.1f;
+    
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                rotY = Quaternion.Euler(0f, - touch.deltaPosition.x * rotateSpeedModifier, 0f);
+                transform.rotation = rotY * transform.rotation;
+            }
+        }
+
+    }
+}
